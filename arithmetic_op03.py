123a2786eab489a61784b84d2e36d945e0bc92d9
asadrasha01 <74987517+asadrasha01@users.noreply.github.com>
2022-12-20 15:21:09 +0200

    01

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,8 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 69
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+last_digit = number % 10
+first_digit = (number - last_digit)/10
+answer = last_digit, first_digit
 #Print the value of the 'answer'
+print(answer)
--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,10 @@
+import math
 #Create a variable called 'number' and assign any value of int to it.
- 
+number = int(48)
 #Divide the number by 2 and assign number.
- 
+number/2
 #Find the fraction of the number and assign to a variable called 'answer'.
- 
+answer = math.modf(number)
 #Print the answer.
- 
+print(answer)
 #Print the number.
